7f0cb9494c8131537351453fd58edfcdc08f3dfd
aidan-parkinson <aidan.parkinson@gmail.com>
2021-01-30 15:21:22 +0000

    json encoding

--- dht11-display.py
+++ dht11-display.py
@@ -1,6 +1,8 @@
 import adafruit_dht
 import time
 import RPi.GPIO as GPIO
+import json
+import datetime
 
 pin = 4
 
@@ -11,9 +13,13 @@ dht_device = adafruit_dht.DHT11(GPIO.input(pin))
 while True:
     humidity = dht_device.humidity
     temperature = dht_device.temperature
-    
-    if humidity is not None and temperature is not None:
-        print("Temp=(0:0.1f)C Humidity=(1:0.1f)%".format(temperature, humidity))
-    else:
-        print("Sensor failure. Check wiring.")
+
+    reading = json.dumps(['humidity': humidity, 'temperature': temperature, 'datetime', datetime.datetime])
+
+    print(reading, sort_keys=True, indent=4)
+    #if humidity is not None and temperature is not None:
+    #    print("Temp=(0:0.1f)C Humidity=(1:0.1f)%".format(temperature, humidity))
+    #else:
+    #    print("Sensor failure. Check wiring.")
+
     time.sleep(3);
