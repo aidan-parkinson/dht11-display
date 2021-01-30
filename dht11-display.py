1f1e16f46a75018b64ec40981fe9731a76b10ff2
aidan-parkinson <aidan.parkinson@gmail.com>
2021-01-30 15:46:59 +0000

    datetime.now

--- dht11-display.py
+++ dht11-display.py
@@ -1,13 +1,10 @@
 import adafruit_dht
 import time
-#import RPi.GPIO as GPIO
 import board
 import json
 import datetime
 
 pin = board.D4
-
-#GPIO.setup(pin, GPIO.IN)
 
 dht_device = adafruit_dht.DHT11(pin)
 
@@ -15,7 +12,7 @@ while True:
     humidity = dht_device.humidity
     temperature = dht_device.temperature
 
-    reading = json.dumps({'humidity': humidity, 'temperature': temperature, 'datetime': datetime.utcnow()})
+    reading = json.dumps({'humidity': humidity, 'temperature': temperature, 'datetime': datetime.now()})
 
     print(reading, sort_keys=True, indent=4)
 
